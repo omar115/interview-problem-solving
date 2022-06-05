2d5217994294f28126c3fe5c06e2632a9b298c64
omar115 <omarhasan115@gmail.com>
2022-06-06 02:55:13 +0600

    leetcode(python): day-1 solved moveZeros e-283

--- easy-283/move_zeros.py
+++ easy-283/move_zeros.py
@@ -1,21 +1,11 @@
 class Solution:
-    def moveZeroes(self, li) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        li2=[]
-        li3=[]
-        for i in range(len(li)):
-            if li[i] != 0:
-                li2.append(li[i])
-            else:
-                li3.append(li[i])
-        for i in range(len(li3)):
-            li2.append(li3[i])
-        print('final result \n ', li2)
-        return li2
-
-
-li = [0, 1, 0, 3, 12]
-a = Solution()
-a.moveZeroes(li)
+    def moveZeroes(self, nums: List[int]) -> None:
+        j = 0
+        n = len(nums)
+        for num in nums:
+            if num != 0:
+                nums[j] = num
+                j += 1
+        
+        for x in range(j, n):
+            nums[x] = 0
