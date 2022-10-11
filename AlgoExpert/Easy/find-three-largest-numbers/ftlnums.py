7899e12c589a5aa6c53701ac80d2549e8f3b6ba4
omar_hasan <omar.hasan@udplatforms.com>
2022-10-11 15:11:51 +0600

    added files

---
+++ AlgoExpert/Easy/find-three-largest-numbers/ftlnums.py
@@ -0,0 +1,6 @@
+"""
+Given an array that contains at least three integers, we are asked to write a function that is going 
+to find the three largest numbers in the input array
+and return them in an array sorted in ascending order. We are not allowed to sort the input array.
+
+"""
