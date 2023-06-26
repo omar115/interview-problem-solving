a022993a947ba4d7167bc7a22e1cef4ea37a1c6f
omar_hasan <omar.hasan@udplatforms.com>
2023-06-27 04:08:28 +0600

    baseball tried

--- Leetcode/L682/baseball.py
+++ Leetcode/L682/baseball.py
@@ -8,5 +8,19 @@ from typing import List
 
 class Solution:
     def calPoints(self, operations: List[str]) -> int:
+        li = []
+        for num in range(len(operations)):
+            if operations[num].isdigit():
+                li.append(int(operations[num]))
+            elif operations[num] == 'C':
+                li.pop()
+            elif operations[num] == 'D':
+                li.append(li[-1] * 2)
+            elif operations[num] == '+':
+                li.append(li[len(li)-1] + li[len(li)-2])
         
-        return 0
+        return sum(li)
+
+
+x = Solution()
+print(x.calPoints(['5', '2', 'C', 'D', '+']))
