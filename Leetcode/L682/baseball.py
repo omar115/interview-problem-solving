2e0496bbfffc0609d5e95f3ae055842dff64f328
omar_hasan <omar.hasan@udplatforms.com>
2023-06-30 19:28:36 +0600

    baseball solved

--- Leetcode/L682/baseball.py
+++ Leetcode/L682/baseball.py
@@ -10,17 +10,17 @@ class Solution:
     def calPoints(self, operations: List[str]) -> int:
         li = []
         for num in range(len(operations)):
-            if operations[num].isdigit():
-                li.append(int(operations[num]))
-            elif operations[num] == 'C':
+            if operations[num] == 'C':
                 li.pop()
             elif operations[num] == 'D':
-                li.append(li[-1] * 2)
+                li.append(int(li[-1]) * 2)
             elif operations[num] == '+':
-                li.append(li[len(li)-1] + li[len(li)-2])
+                li.append(int(li[len(li)-1]) + int(li[len(li)-2]))
+            else:
+                li.append(int(operations[num]))
         
         return sum(li)
 
 
 x = Solution()
-print(x.calPoints(['5', '2', 'C', 'D', '+']))
+print(x.calPoints(["5","-2","4","C","D","9","+","+"]))
